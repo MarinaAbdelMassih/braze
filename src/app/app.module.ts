a1b6b595f345ca1d4f41996bca3c52cf85351a3b
Marina <marina.abdelmassih@almentor.net>
2021-04-08 18:52:58 +0200

    hhhhh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {QuillModule} from 'ngx-quill';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot() ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
